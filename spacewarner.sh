2f895bff6edd3e0157c70e29593bdb3fecac2881
Harald Glatt <code@hach.re>
2018-03-13 17:45:23 +0100

    1.4.20180313.1
    added: support for ssmtp
    added: cfgMailService configuration option to define whether msmtp or ssmtp is to be used
    added: significant parameter checks
    added: ability to disable the entire mail warning system by setting cfgMailService to 'none'

--- spacewarner.sh
+++ spacewarner.sh
@@ -5,20 +5,28 @@
 #
 
 # URL: https://github.com/hachre/spacewarner
-# Version: 1.3.20180312.7
+# Version: 1.4.20180313.1
 
 
 #
 # Configuration
 #
 
+# Supported services are "msmtp", "ssmtp" and "none"
+cfgMailService="msmtp"
+
+# Required for both msmtp and ssmtp
 cfgMailTo="hachre@dynaloop.com"
 cfgMailFrom="solaris@dynaloop.com"
 cfgMailFromName="Solaris"
+
+# Required for just msmtp, leave empty or comment out for ssmtp
 cfgMailFromHost="solaris.dynaloop.com"
 cfgMailServer="orion.dynaloop.net"
 cfgMailUser="server_mail@dynaloop.com"
 cfgMailPassFile="/root/.mailpass"
+
+# General configuration options
 cfgWarnBelow="10"
 
 
@@ -31,30 +39,90 @@ function help {
 	echo " --cron:     Omits all output."
 	echo " --mailtest: Send a test mail."
 }
-if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
-	help
-	exit 127
-fi
 
-function checks {
-	which msmtp 1>/dev/null 2>&1
-	if [ "$?" != "0" ]; then
-		echo "Error: 'msmtp' is not installed or not in PATH."
+function parameterChecks {
+	if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+		help
+		exit 127
+	fi
+	if [ -z "$cfgMailService" ]; then
+		echo "Error: Config value 'cfgMailService' has to be set to either 'msmtp' or 'ssmtp'."
 		exit 1
 	fi
+	if [ "$cfgMailService" != "msmtp" ] && [ "$cfgMailService" != "ssmtp" ]; then
+		echo "Error: Config value 'cfgMailService' has to be set to either 'msmtp' or 'ssmtp'."
+		exit 1
+	fi
+	if [ -z "$cfgMailTo" ]; then
+		echo "Error: Config value 'cfgMailTo' has to be set to a valid recipient mail address."
+		exit 1
+	fi
+	if [ -z "$cfgMailFrom" ]; then
+		echo "Error: Config value 'cfgMailFrom' has to be set to a valid sender mail address."
+		exit 1
+	fi
+	if [ -z "$cfgMailFromName" ]; then
+		echo "Error: Config value 'cfgMailFromName' should be set to a sender name."
+		exit 1
+	fi
+	if [ "$cfgMailService" == "msmtp" ]; then
+		if [ -z "$cfgMailFromHost" ]; then
+			echo "Error: Config value 'cfgMailFromHost' has to be set to a valid sender hostname."
+			exit 1
+		fi
+		if [ -z "$cfgMailServer" ]; then
+			echo "Error: Config value 'cfgMailServer' has to be set to a SMTP mail server hostname."
+			exit 1
+		fi
+		if [ -z "$cfgMailUser" ]; then
+			echo "Error: Config value 'cfgMailUser' has to be set to a SMTP authentication username."
+			exit 1
+		fi
+		if [ -z "$cfgMailPassFile" ]; then
+			echo "Error: Config value 'cfgMailPassFile' has to be set to the location of the file containing the SMTP authentication password."
+			exit 1
+		fi
+		which msmtp 1>/dev/null 2>&1
+		if [ "$?" != "0" ]; then
+			echo "Error: Config value 'cfgMailService' defines 'msmtp' as your desired service, but it is not installed and not in PATH."
+			exit 1
+		fi
+	fi
+	if [ "$cfgMailService" == "ssmtp" ]; then
+		which ssmtp 1>/dev/null 2>&1
+		if [ "$?" != "0" ]; then
+			echo "Error: Config value 'cfgMailService' defines 'ssmtp' as your desired service, but it is not installed and not in PATH."
+			exit 1
+		fi
+	fi
 }
-checks
+parameterChecks $*
 
 function mail {
-	# add -P after --auth=on to enable debugging msmtp
-	echo -e "From: $cfgMailFromName <$cfgMailFrom>\nSubject: Low Disk Space Warning\n$*" | msmtp --host=$cfgMailServer --user=$cfgMailUser --passwordeval="cat $cfgMailPassFile" --from=$cfgMailFrom --domain=$cfgMailFromHost --tls=on --tls-certcheck=off --auth=on $cfgMailTo
-	return $?
+	if [ "$cfgMailService" == "msmtp" ]; then
+		# add -P after --auth=on to enable debugging msmtp
+		echo -e "From: $cfgMailFromName <$cfgMailFrom>\nSubject: Low Disk Space Warning\n$*" | msmtp --host=$cfgMailServer --user=$cfgMailUser --passwordeval="cat $cfgMailPassFile" --from=$cfgMailFrom --domain=$cfgMailFromHost --tls=on --tls-certcheck=off --auth=on $cfgMailTo
+		return $?
+	fi
+	if [ "$cfgMailService" == "ssmtp" ]; then
+		echo -e "To: $cfgMailTo\nFrom: $cfgMailFromName <$cfgMailFrom>\nSubject: Low Disk Space Warning\n$*" | ssmtp $cfgMailTo
+		return $?
+	fi
+	if [ "$cfgMailService" == "none" ]; then
+		return 0
+	fi
+	echo "Warning: 'cfgMailService' has not been set correctly. Unknown value: '$cfgMailService' ignored."
+	return 0
 }
 
 function mailtest {
-	echo "Sending a test mail to '$cfgMailTo'..."
-	mail "Low Disk Space Warner has successfully reached you!"
-	exit $?
+	if [ "$cfgMailService" == "msmtp" ] || [ "$cfgMailService" == "ssmtp" ]; then
+		echo "Sending a test mail to '$cfgMailTo'..."
+		mail "Low Disk Space Warner has successfully reached you!"
+		exit $?
+	fi
+	echo "Error: 'cfgMailService' has been set to 'none' or an unknown value. Mail warnings are disabled."
+	exit 1
 }
 if [ "$1" == "--mailtest" ] || [ "$1" == "--testmail" ]; then
 	mailtest
@@ -78,7 +146,7 @@ for entry in $(df -x tmpfs -x devtmpfs --output=source,size,avail | tail -n+2); 
 		ok="[BAD]"
 		alarm $source $percentFree
 	fi
-	if [ "$1" != "--cron" ]; then
+	if [ "$1" != "--cron" ] && [ "$1" != "-c" ]; then
 		echo "$ok $source: ${percentFree}% (size: $size, avail: $avail)"
 	fi
 done
